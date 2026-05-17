1ce6e2b6649fa1efd5e90ecc690e34a004429a9c
Rodlomo <rodlomo04@gmail.com>
2026-05-16 21:35:08 -0600

    Torch light effect
    Added torch light effect script for dynamic light in player (there's an example added on old level one)

---
+++ Un-Tile-ted Project/Assets/Scripts/TorchLightFlicker.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class TorchLightFlicker : MonoBehaviour
+{
+    private Light torchLight;
+
+    [Header("Base Settings")]
+    [SerializeField] private float baseIntensity = 2f;
+    [SerializeField] private float baseRange = 8f;
+
+    [Header("Flicker Settings")]
+    [SerializeField] private float intensityVariation = 0.3f;
+    [SerializeField] private float rangeVariation = 0.5f;
+
+    [SerializeField] private float flickerSpeed = 8f;
+
+    private float randomOffset;
+
+    void Start()
+    {
+        torchLight = GetComponent<Light>();
+
+        randomOffset = Random.Range(0f, 100f);
+
+        torchLight.intensity = baseIntensity;
+        torchLight.range = baseRange;
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(Time.time * flickerSpeed, randomOffset);
+
+        torchLight.intensity =
+            baseIntensity +
+            (noise - 0.5f) * intensityVariation;
+
+        torchLight.range =
+            baseRange +
+            (noise - 0.5f) * rangeVariation;
+    }
+}
